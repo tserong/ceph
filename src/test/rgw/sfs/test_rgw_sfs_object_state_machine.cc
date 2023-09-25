a928a4a1f357d5f88890a92f9f44bc0e6ebb2177
Tim Serong <tserong@suse.com>
2023-09-25 20:29:51 +1000

    rgw/sfs: testing: use SFStore's DBConnRef where available
    If we're alrady using an SFStore, don't create a new DBConnRef, just use
    the one that's already part of the SFStore.
    
    Signed-off-by: Tim Serong <tserong@suse.com>

--- src/test/rgw/sfs/test_rgw_sfs_object_state_machine.cc
+++ src/test/rgw/sfs/test_rgw_sfs_object_state_machine.cc
@@ -83,7 +83,7 @@ class TestSFSObjectStateMachine : public ::testing::Test {
   }
 
   fs::path getDBFullPath() const { return getDBFullPath(getTestDir()); }
-  sqlite::DBConnRef dbconn() { return std::make_shared<sqlite::DBConn>(cct); }
+  sqlite::DBConnRef dbconn() { return store->db_conn; }
   sqlite::Storage storage() { return dbconn()->get_storage(); }
   ObjectState database_object_state(ObjectRef obj) {
     return storage()
