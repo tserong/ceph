tserong/ceph

commit b6669263cacd56ff589529784066b9f637f79e87

cmake: workaround _num_cores being 0 under qemu emulation
If you're doing something weird like trying to cross compile bits
of ceph using qemu emulation, cmake can think there are zero CPU
cores.  This is because cmake counts "processor" lines in
/proc/cpuinfo, which don't exist when running under qemu-aarch64:

  # cat /proc/cpuinfo
  Processor       : ARMv7 Processor rev 5 (v7l)
  BogoMIPS        : 799.53
  Features        : swp half thumb fastmult vfp edsp thumbee neon vfpv3
  CPU implementer : 0x41
  CPU architecture: 7
  CPU variant     : 0x2
  CPU part        : 0xc08
  CPU revision    : 5

  Hardware        : Genesi Efika MX (Smarttop)
  Revision        : 51030
  Serial          : 0000000000000000

This in turn causes heavy_compile_job_pool to not be set, which
later makes the build fail, as that pool is referenced in
src/tools/ceph-dencoder/CMakeLists.txt.  We can work around the
problem by setting _num_cores to 1 in this case.

Signed-off-by: Tim Serong <tserong@suse.com>

cmake/modules/LimitJobs.cmake
@@ -2,6 +2,11 @@ set(MAX_COMPILE_MEM 3500 CACHE INTERNAL "maximum memory used by each compiling j
 set(MAX_LINK_MEM 4500 CACHE INTERNAL "maximum memory used by each linking job (in MiB)")
 
 cmake_host_system_information(RESULT _num_cores QUERY NUMBER_OF_LOGICAL_CORES)
+# This will never be zero on a real system, but it can be if you're doing
+# weird things like trying to cross-compile using qemu emulation.
+if(_num_cores EQUAL 0)
+  set(_num_cores 1)
+endif()
 cmake_host_system_information(RESULT _total_mem QUERY TOTAL_PHYSICAL_MEMORY)
 
 math(EXPR _avg_compile_jobs "${_total_mem} / ${MAX_COMPILE_MEM}")

commit d20b0f76b61f139ddef4be1b820665a17067ad8c

cmake: use `nproc` if _num_cores is zero
The previous workaround set _num_cores to 1 if cmake thought
NUMBER_OF_LOGICAL_CORES was zero.  That worked fine, but it'd
be much better to take advantage of all possible cores.  This
version runs `nproc` which seems to work just fine under
qemu-aarch64 and gives us the actual number of CPUs.

Fixes: b6669263cacd56ff589529784066b9f637f79e87
Signed-off-by: Tim Serong <tserong@suse.com>

cmake/modules/LimitJobs.cmake
@@ -5,7 +5,11 @@ cmake_host_system_information(RESULT _num_cores QUERY NUMBER_OF_LOGICAL_CORES)
 # This will never be zero on a real system, but it can be if you're doing
 # weird things like trying to cross-compile using qemu emulation.
 if(_num_cores EQUAL 0)
-  set(_num_cores 1)
+  execute_process(
+    COMMAND "nproc"
+    OUTPUT_VARIABLE _num_cores
+    OUTPUT_STRIP_TRAILING_WHITESPACE)
+  message(WARNING "Unable to query NUMBER_OF_LOGICAL_CORES, defaulting to nproc (${_num_cores})")
 endif()
 cmake_host_system_information(RESULT _total_mem QUERY TOTAL_PHYSICAL_MEMORY)
 